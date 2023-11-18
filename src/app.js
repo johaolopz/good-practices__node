6300fc99871dd41338321fceb5fdd84f125c60b6
Johao Lopez <johao.lopz.m@gmail.com>
2023-11-18 12:24:27 -0500

    callback test

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
-console.log('Hola mundo cristiano')
+const arr = ['one', 'two', 'four', 'three']
+
+const [,,three] = arr
+
+const func = {
+    added: (num, cb) => {
+        if (typeof num !== "number") {
+            return cb(`${num} not is a number (NaN)`)
+        }
+        return ++num
+    }
+}
+
+console.log('Using callback: ', func.added('3', (error)=>{
+    if (error) {
+        return error
+    }
+}))
+console.log('Callback test')
